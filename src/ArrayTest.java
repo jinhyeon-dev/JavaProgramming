f071af0fa42540dd40e028117ba4f657bb8d586d
JinhyeonE07 <kjh101807@gmail.com>
2023-10-30 20:03:36 +0900

    first comit

--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -3,5 +3,13 @@ import java.util.Scanner;
 public class ArrayTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        String[] name = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+        double[] score = new double[10];
+
+        for(int i = 0; i<name.length;i++) {
+            System.out.println(name[i] + " 학생의 점수를 입력하세요");
+            score[i] = sc.nextDouble();
+        }
     }
 }
