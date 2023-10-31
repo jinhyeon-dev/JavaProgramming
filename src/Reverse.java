c121736eeb329171e486918eee036fd54eeec51c
jinhyeon-dev <kjh101807@gmail.com>
2023-10-31 12:12:21 +0900

    commit

---
+++ src/Reverse.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int[] n = new int[100];
+
+        for (int i = 0; i < n.length; i++) {
+            n[i] = sc.nextInt();
+            for (int j = n.length; j > i; j--) {
+
+            }
+        }
+    }
+}
